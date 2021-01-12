4587c28d5f473127330f043e727c575bae36cc97
Gustavo Luna <gustavo.luna@grupoabsa.com>
2021-01-12 00:33:37 -0600

    adding Vagrantfiles

---
+++ README.md
@@ -0,0 +1,35 @@
+# vagrant
+
+Vagrant and corresponding Vagrantfiles
+
+## All commands should be executed inside the Vagrant directory (where the Vagrantfile is located)
+
+Start virtual machine
+
+```
+vagrant up
+```
+
+SSH into virtual machine
+
+```
+vagrant ssh
+```
+
+Stop virtual machine
+
+```
+vagrant halt
+```
+
+Destroy your virtual machine
+
+```
+vagrant destroy
+```
+
+Reload the virtual machine
+
+```
+vagrant reload
+```
---
+++ template/bootstrap_centos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "Set root password"
+echo "12qwaszx" | passwd --stdin root >/dev/null 2>&1
+
+# Set local user account
+echo "Set up local user account"
+useradd -m -s /bin/bash lunag
+echo "12qwaszx" | passwd --stdin lunag >/dev/null 2>&1
+echo "lunag ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Update bashrc file
+echo "export TERM=xterm" >> /etc/bashrc
