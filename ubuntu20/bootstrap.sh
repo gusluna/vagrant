9c6361d767085542503d68dcc90a355ac906e32b
Gustavo Luna <gusluna@gmail.com>
2021-01-12 21:52:23 -0600

    add user to vagrant group

--- ubuntu20/bootstrap.sh
+++ ubuntu20/bootstrap.sh
@@ -15,6 +15,7 @@ echo "Set up local user account"
 useradd -m -s /bin/bash lunag
 echo -e "12qwaszx" | passwd lunag >/dev/null 2>&1
 echo "lunag ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo usermod -aG vagrant lunag
 
 # Update bashrc file
 echo "export TERM=xterm" >> /etc/bashrc
